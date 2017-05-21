3877b172b23fcafaba8cb3116a98a6a07554c51b
DomainFlag <Cchivriga@hotmail.com>
2017-05-21 16:56:06 +0200

    Some small changes.

--- app/src/main/java/com/example/cchiv/belot/MainActivity.java
+++ app/src/main/java/com/example/cchiv/belot/MainActivity.java
@@ -19,16 +19,30 @@ public class MainActivity extends AppCompatActivity {
 
     public void setTextA(View view) {
         EditText T = (EditText) findViewById(R.id.numberA);
+        TextView textViewGameHistoryA = (TextView) findViewById(R.id.gameHistoryA);
+        TextView textViewTextTotalA = (TextView) findViewById(R.id.textTotalA);
+        textViewGameHistoryA.setVisibility(View.VISIBLE);
+        textViewTextTotalA.setVisibility(View.VISIBLE);
         String text = T.getText().toString();
-        int score = Integer.parseInt(text);
-        setTotalA(score);
+        if (!text.isEmpty()) {
+            int score = Integer.parseInt(text);
+            setTotalA(score);
+        }
+        T.setText(null);
     }
 
     public void setTextB(View view) {
         EditText T = (EditText) findViewById(R.id.numberB);
+        TextView textViewGameHistoryB = (TextView) findViewById(R.id.gameHistoryB);
+        TextView textViewTextTotalB = (TextView) findViewById(R.id.textTotalB);
+        textViewGameHistoryB.setVisibility(View.VISIBLE);
+        textViewTextTotalB.setVisibility(View.VISIBLE);
         String text = T.getText().toString();
-        int score = Integer.parseInt(text);
-        setTotalB(score);
+        if (!text.isEmpty()) {
+            int score = Integer.parseInt(text);
+            setTotalB(score);
+        }
+        T.setText(null);
     }
 
     public void reset(View view) {
@@ -39,29 +53,29 @@ public class MainActivity extends AppCompatActivity {
         TA.setText(newText);
         TextView TB = (TextView) findViewById(R.id.textTotalB);
         TB.setText(newText);
-        TextView HA = (TextView) findViewById(R.id.historyA);
+        TextView HA = (TextView) findViewById(R.id.gameHistoryA);
         HA.setText(null);
-        TextView HB = (TextView) findViewById(R.id.historyB);
+        TextView HB = (TextView) findViewById(R.id.gameHistoryB);
         HB.setText(null);
     }
 
     public void setTotalA(int number) {
         TextView T = (TextView) findViewById(R.id.textTotalA);
-        TextView H = (TextView) findViewById(R.id.historyA);
+        TextView H = (TextView) findViewById(R.id.gameHistoryA);
         sumA += number;
         String newText = "TOTAL: " + sumA + "p";
         T.setText(newText);
-        String newHistoryEntry = H.getText().toString() + " " + number + " ";
+        String newHistoryEntry = H.getText().toString() + number + " ";
         H.setText(newHistoryEntry);
     }
 
     public void setTotalB(int number) {
         TextView T = (TextView) findViewById(R.id.textTotalB);
-        TextView H = (TextView) findViewById(R.id.historyB);
+        TextView H = (TextView) findViewById(R.id.gameHistoryB);
         sumB += number;
         String newText = "TOTAL: " + sumB + "p";
         T.setText(newText);
-        String newHistoryEntry = H.getText().toString() + " " + number + " ";
+        String newHistoryEntry = H.getText().toString() + number + " ";
         H.setText(newHistoryEntry);
     }
 }
